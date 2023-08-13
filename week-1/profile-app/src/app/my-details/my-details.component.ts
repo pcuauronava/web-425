6e38753c6badbf31e5a0facfe24228cfa2b1f807
Patrick Cuauro <pcuauronava@my365.bellevue.edu>
2023-08-12 21:13:02 -0400

    Added Prolife-app with angular cli
    added the profile-app with angular, added the modules my-details and my-image, added my profile pic from whatsapp and followed the instructions

---
+++ week-1/profile-app/src/app/my-details/my-details.component.ts
@@ -0,0 +1,17 @@
+/**
+ * Title: my-details.component.ts
+ * Author: Professor Krasso
+ * Date: Aug 12 2023
+ * Description: My Details component
+ */
+
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-my-details',
+  templateUrl: './my-details.component.html',
+  styleUrls: ['./my-details.component.css']
+})
+export class MyDetailsComponent {
+
+}
---
+++ week-1/profile-app/src/app/my-image/my-image.component.ts
@@ -0,0 +1,17 @@
+/**
+ * Title: my-image.component.ts
+ * Author: Professor Krasso
+ * Date: Aug 12 2023
+ * Description: My Image component
+ */
+
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-my-image',
+  templateUrl: './my-image.component.html',
+  styleUrls: ['./my-image.component.css']
+})
+export class MyImageComponent {
+
+}
