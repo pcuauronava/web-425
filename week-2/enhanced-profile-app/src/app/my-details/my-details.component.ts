2e8a46b38a0694020f80e27178e748b9f885ae35
Patrick Cuauro <pcuauronava@my365.bellevue.edu>
2023-08-16 19:38:51 -0400

    Added enhanced-profile-app
    Tested ngIf, ngFor

---
+++ week-2/enhanced-profile-app/src/app/my-details/my-details.component.ts
@@ -0,0 +1,41 @@
+import { Component, OnInit } from '@angular/core';
+
+// new class
+export default class Person {
+  fullName: string;
+  favoriteFood: string;
+  favoriteColor: string;
+  keywords = [
+    "#TypeScript", "#2020", "#CodingWithAngular", "#ngOmaha"
+  ];
+
+constructor(fullName: string, favoriteFood: string, favoriteColor: string) {
+  this.fullName = fullName;
+  this.favoriteFood = favoriteFood;
+  this.favoriteColor = favoriteColor;
+  }
+
+  toString() {
+    console.log(`\n Full name: ${this.fullName}\n Favorite food: ${this.favoriteFood}\n Favorite color: ${this.favoriteColor}`);
+    }
+}
+
+@Component({
+  selector: 'app-my-details',
+  templateUrl: './my-details.component.html',
+  styleUrls: ['./my-details.component.css']
+})
+// Modified class
+export class MyDetailsComponent implements OnInit {
+
+  myProfile: Person;
+
+  constructor() {
+    this.myProfile = new Person("Patrick Cuauro", "Sushi", "Red");
+    this.myProfile.toString();
+    // This makes the console.log statement in the Person class work
+  }
+
+ngOnInit(): void {
+  }
+}
